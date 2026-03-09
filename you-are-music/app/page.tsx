bcb7c90e34a2a93735f9a3a3a89f93bdc92a261f
Mihai Danaila <danailateodor@yahoo.com>
2026-03-09 20:35:20 +0200

    main app on home page instead of /test

--- you-are-music/app/page.tsx
+++ you-are-music/app/page.tsx
@@ -1,65 +1,99 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+import HandTracker from '@/components/model/mediapipeModel';
+import { ModelPrediction, PredictionBox } from '@/components/model/mediapipeModel';
+import Webcam from '@/components/webcam/webcam';
+import { Landmark } from '@mediapipe/tasks-vision';
+import { useEffect, useRef, useState } from 'react';
+
+
+/**
+  * 
+  * @param {PredictionBox} predictionBox
+  * @param {HTMLCanvasElement} canvas 
+  * @returns - None
+  */
+
+function drawPredictionBox(predictionBox: PredictionBox, canvas: HTMLCanvasElement){
+    const canvasContext = canvas.getContext("2d");
+
+    if(!canvasContext){
+        return;
+    }
+
+    canvasContext.strokeStyle = "red";
+    canvasContext.beginPath()
+    canvasContext.rect(
+        predictionBox.x - predictionBox.width / 2,
+        predictionBox.y - predictionBox.height / 2,
+        predictionBox.width,
+        predictionBox.height,
+    )
+
+    canvasContext.stroke();
+ }
+
+ function drawPredictionKeypoints(features: Landmark[], canvas: HTMLCanvasElement){
+    const canvasContext = canvas.getContext("2d");
+    if(!canvasContext) return;
+
+    canvasContext.fillStyle = "green";
+
+    features.forEach(landmark => {
+        canvasContext.beginPath();
+        canvasContext.arc(landmark.x, landmark.y, 5, 0, 2*Math.PI);
+        canvasContext.fill();
+    })
+ }
+
+function drawPrediction(prediction: ModelPrediction, canvas: HTMLCanvasElement){
+    const canvasContext = canvas.getContext("2d");
+    if(!canvasContext) return;
+    
+    canvasContext.clearRect(0,0,canvas.width, canvas.height);
+    if (prediction.predictionBox)
+        drawPredictionBox(prediction.predictionBox, canvas);
+    
+    drawPredictionKeypoints(prediction.features,canvas);
+    
+}
+
+export default function Test() {
+    const webcamCanvas = useRef<HTMLCanvasElement | null>(null);
+    const overlayCanvas = useRef<HTMLCanvasElement | null>(null);
+    const videoStream = useRef<HTMLVideoElement | null>(null);
+
+    const [prediction, setPrediction] = useState<ModelPrediction | null>(null);
+
+    useEffect(() => {
+        if(!overlayCanvas.current){
+            return;
+        }
+
+        if(!prediction){
+            const canvasContext = overlayCanvas.current.getContext("2d");
+            canvasContext?.clearRect(0,0,overlayCanvas.current.width, 
+                overlayCanvas.current.height);
+            return;
+        }
+        drawPrediction(prediction, overlayCanvas.current);
+    
+    }, [prediction])
+    
+    return(
+        <>
+        <HandTracker 
+        videoStream={videoStream}
+        setPrediction={setPrediction}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        {/* <ModelRunner webcamCanvasRef={webcamCanvas} setPrediction={setPrediction}/> */}
+
+        <div className='relative'>
+            <canvas 
+            ref = {overlayCanvas}
+            width={640} height={640} className='absolute top-0 left-0 '></canvas>
+            <Webcam videoRef={videoStream}/>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+        </>
+    )
 }
